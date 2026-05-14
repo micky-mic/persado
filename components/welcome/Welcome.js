7096fe2774f65ad5a79ce7c4ed0824b83cc78c74
oliverdev0426 <oliverdev0426@gmail.com>
2026-05-14 17:43:21 +0530

    welcome page done

--- components/welcome/Welcome.js
+++ components/welcome/Welcome.js
@@ -1,42 +1,49 @@
 import Image from "next/image"
 import Link from "next/link";
-import white_logo from "@/public/related_assets/logo/white_logo.svg";
+import logo from "@/public/new/logo.png";
+import welphone from "@/public/new/welphone.png";
+import welbg from "@/public/new/welbg.png";
 
 const Welcome = () => {
     return (
         <section className="welcome-page-section page_animation">
-            <div className="bg-video-wrapper">
-                <video
-                    className="backgroundVideo"
-                    muted
-                    autoPlay
-                    loop
-                    playsInline
-                >
-                    <source
-                        src="/related_assets/welcome_video.mp4"
-                        type="video/mp4"
-                    />
-                </video>
-            </div>
-            <div className="welcome-logo">
-                <Image
-                    src={white_logo}
-                    alt="logo"
-                    height={100}
-                    width={100}
-                    unoptimized
-                />
-            </div>
-            <div className="welcome-content">
-                <h3 className="playfair-font">
-                    Celebrating the communities who inspire and uplift us every day
-                </h3>
-            </div>
-            <div className="start-btn">
-                <Link href="/signin">
-                    <button>LET’S GET STARTED</button>
-                </Link>
+            <div
+                className="welcome-page-wrapper"
+                style={{
+                    backgroundImage: `url(${welbg.src})`,
+                }}
+            >
+                <div className="welcome-overlay"></div>
+
+                <div className="welcome-inner">
+                    <div className="welcome-img">
+                        <Image
+                            src={welphone}
+                            alt="logo"
+                            height={100}
+                            width={100}
+                            unoptimized
+                        />
+                    </div>
+                    <div className="welcome-logo">
+                        <Image
+                            src={logo}
+                            alt="logo"
+                            height={100}
+                            width={100}
+                            unoptimized
+                        />
+                    </div>
+
+                    <div className="welcome-content">
+                        <h3>
+                            Nexos.ai an all-in-one AI marketing platform built to help businesses work faster and grow easier. Create content, manage campaigns, automate tasks, and keep your marketing operations in one streamlined system.
+                        </h3>
+                    </div>
+                    <Link href="/signin">
+                        <button className="primary-btn">Get Started</button>
+                    </Link>
+                </div>
             </div>
         </section>
     )
