1e2d4251eaefd1e15bf776146300e3c466c06525
steveescobar <steveescobar@gmail.com>
2026-07-18 20:14:36 +0300

    Initial commit

--- components/auth/SignUp.js
+++ components/auth/SignUp.js
@@ -151,7 +151,7 @@ const SignUp = () => {
                             ></path>
                         </svg>
                     </div>
-                    {/* <div className="app-form-group app-form-group-include-conf">
+                    <div className="app-form-group app-form-group-include-conf">
                         <input
                             type="text"
                             placeholder="Email Address"
@@ -168,7 +168,7 @@ const SignUp = () => {
                         >
                             <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4l217.6 163.2c11.4 8.5 27 8.5 38.4 0l217.6-163.2c12.1-9.1 19.2-23.3 19.2-38.4 0-26.5-21.5-48-48-48zM0 176v208c0 35.3 28.7 64 64 64h384c35.3 0 64-28.7 64-64V176L294.4 339.2a63.9 63.9 0 0 1-76.8 0z"></path>
                         </svg>
-                    </div> */}
+                    </div>
                     <label>Password</label>
                     <div className="app-form-group app-form-group-include-conf">
                         <input
