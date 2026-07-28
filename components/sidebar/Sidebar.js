e9cefc0c35946439e0202618f46d586e9cfc9e8a
Mic <michalmichal202000@gmail.com>
2026-07-29 04:04:36 +0530

    all corrections

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import ConfirmModal from '../successModal/ConfirmModal';
 import Pop from '../notification/Pop';
 import logo from '@/public/new2/logo.png'
+import menubar from '@/public/new2/sidebar/menubar.png'
 import sidelogo from '@/public/new2/sidelogo.png'
 import vip1 from '@/public/new/vip1.png'
 import vip2 from '@/public/new/vip2.png'
